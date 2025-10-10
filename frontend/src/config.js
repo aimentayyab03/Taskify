474718ba2fc2364f4a7f8b1a958e0a303f8e1cb8
Aimen Tayyab <109237494+aimentayyab03@users.noreply.github.com>
2025-10-10 17:33:28 +0500

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,14 +1,10 @@
-// src/config.js
-
 let backendUrl;
 
-if (window.location.hostname === 'localhost') {
-  // Local development
+if (process.env.NODE_ENV === 'development') {
   backendUrl = 'http://localhost:5000';
-}
 } else {
   // Default (production, e.g., Elastic Beanstalk)
-   backendUrl = 'http://13.53.187.249:5000';
+  backendUrl = 'http://13.53.187.249:5000';
 }
 
-export const BACKEND_URL = backendUrl;
+export default backendUrl;
