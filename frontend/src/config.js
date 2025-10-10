4b53e23ac9a00063eca5c410615281ad36897326
Aimen Tayyab <109237494+aimentayyab03@users.noreply.github.com>
2025-10-10 15:55:11 +0500

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -5,9 +5,7 @@ let backendUrl;
 if (window.location.hostname === 'localhost') {
   // Local development
   backendUrl = 'http://localhost:5000';
-} else if (window.location.hostname === '13.53.187.249') {
-  // EC2 deployment
-  backendUrl = 'http://13.53.187.249:5000';
+}
 } else {
   // Default (production, e.g., Elastic Beanstalk)
    backendUrl = 'http://13.53.187.249:5000';
