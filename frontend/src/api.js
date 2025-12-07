eaed0e794e2b510e6f0f7633df97a1bf3b39d724
E L I T E B O O K <aimentayyab215@gmail.com>
2025-12-07 14:14:35 +0500

    Describe your changes here

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,6 +2,7 @@
 import axios from 'axios';
 
 // Use environment variable or fallback to the new EC2 public IP
-const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://13.60.172.200:5001/api";
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://16.171.26.154:5000/api";
+
 
 export default BASE_URL;
